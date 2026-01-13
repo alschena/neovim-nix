45b7deac8f861d0fed77062c90478ef71e9c6f7f
Alessandro Schena <alessandro.schena.97@protonmail.com>
2026-01-13 16:23:08 +0100

    feat: enable grammar language server

---
+++ nvim/plugin/grammar.lua
@@ -0,0 +1,69 @@
+--- From [nvim-lspconfig](https://github.com/neovim/nvim-lspconfig/blob/master/lsp/ltex.lua)
+
+if vim.g.did_load_grammar_language_server then
+  return
+end
+vim.g.did_load_grammar_language_server = true
+
+
+local language_id_mapping = {
+  bib = 'bibtex',
+  plaintex = 'tex',
+  rnoweb = 'rsweave',
+  rst = 'restructuredtext',
+  tex = 'latex',
+  pandoc = 'markdown',
+  text = 'plaintext',
+}
+
+local filetypes = {
+  'bib',
+  'gitcommit',
+  'markdown',
+  'org',
+  'plaintex',
+  'rst',
+  'rnoweb',
+  'tex',
+  'pandoc',
+  'quarto',
+  'rmd',
+  'context',
+  'html',
+  'xhtml',
+  'mail',
+  'text',
+}
+
+local function get_language_id(_, filetype)
+  local language_id = language_id_mapping[filetype]
+  if language_id then
+    return language_id
+  else
+    return filetype
+  end
+end
+local enabled_ids = {}
+do
+  local enabled_keys = {}
+  for _, ft in ipairs(filetypes) do
+    local id = get_language_id({}, ft)
+    if not enabled_keys[id] then
+      enabled_keys[id] = true
+      table.insert(enabled_ids, id)
+    end
+  end
+end
+
+vim.lsp.config['ltex-ls'] = {
+  cmd = { 'ltex-ls' },
+  filetypes = filetypes,
+  root_markers = { '.git' },
+  get_language_id = get_language_id,
+  settings = {
+    ltex = {
+      enabled = enabled_ids,
+    },
+  },
+}
+vim.lsp.enable('ltex-ls')
