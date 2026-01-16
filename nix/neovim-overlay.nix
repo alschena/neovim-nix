2c096e9361b9a361ea7399bb76f26581942f0064
Alessandro Schena <alessandro.schena.97@protonmail.com>
2026-01-16 21:15:17 +0100

    refactor: reorganize neovim plugins

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -75,8 +75,6 @@ with final.pkgs.lib; let
     # ^ Useful utilities
     # libraries that other plugins depend on
     sqlite-lua
-    sonarlint-nvim
-    rustaceanvim
     plenary-nvim
     nvim-web-devicons
     vim-repeat
@@ -85,6 +83,10 @@ with final.pkgs.lib; let
     # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
     which-key-nvim
+    # Linting
+    sonarlint-nvim
+    # Rust
+    rustaceanvim
   ];
 
   extraPackages = with pkgs; [
