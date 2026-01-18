3e20c4a646e8b955938cf17b697d66e447a81622
alschena <146067245+alschena@users.noreply.github.com>
2026-01-18 09:01:46 +0000

    Update ty.lua: enable ruff

--- nvim/plugin/ty.lua
+++ nvim/plugin/ty.lua
@@ -1,6 +1,7 @@
-if vim.g.did_load_python_type_checker then
+if vim.g.did_load_python_configuration then
   return
 end
-vim.g.did_load_python_type_checker = true
+vim.g.did_load_python_configuration = true
 
 vim.lsp.enable('ty')
+vim.lsp.enable('ruff')
