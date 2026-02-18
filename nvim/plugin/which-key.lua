3e0719d69b46bdbad637b85d13d8bce141657171
Alessandro Schena <alessandro.schena.97@protonmail.com>
2026-02-18 12:13:02 +0100

    update: change git keybindings.

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -4,6 +4,7 @@ require('which-key').setup {
 }
 vim.keymap.set('n', '<leader>f', '<Nop>', { desc = '+Find' })
 vim.keymap.set('n', '<leader>g', '<Nop>', { desc = '+Git' })
+vim.keymap.set('n', '<leader>g~', '<Nop>', { desc = '+Toggle' })
 vim.keymap.set('n', ',', '<Nop>', { desc = '+Language' })
 vim.keymap.set('n', ',~', '<Nop>', { desc = '+toggle' })
 vim.keymap.set('n', ',p', '<Nop>', { desc = '+Peek' })
